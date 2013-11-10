7c80b9cd9914ea88c27dea577d5c6186ffe3e0e7
cleentfaar <leentfaar87@gmail.com>
2013-11-10 01:07:15 +0100

    Totally refactored codebase to suit conventions

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,69 +1,214 @@
+/*global module: false*/
+/*jslint indent: 2*/
+
 module.exports = function(grunt) {
 
-	grunt.initConfig({
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
 
-		// Import package manifest
-		pkg: grunt.file.readJSON("skinswitcher.jquery.json"),
+    meta: {
+      name: 'skinswitcher',
+      banner: '/**\n' +
+        ' * <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+        ' * <%= pkg.homepage %>\n' +
+        ' *\n' +
+        ' * <%= pkg.description %>\n' +
+        ' *\n' +
+        ' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+        ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %>\n' +
+        ' */\n' +
+        '\n'
+    },
 
-		// Banner definitions
-		meta: {
-			banner: "/*\n" +
-				" *  <%= pkg.title || pkg.name %> - v<%= pkg.version %>\n" +
-				" *  <%= pkg.description %>\n" +
-				" *  <%= pkg.homepage %>\n" +
-				" *\n" +
-				" *  Made by <%= pkg.author.name %>\n" +
-				" *  Under <%= pkg.licenses[0].type %> License\n" +
-				" */\n"
-		},
+    concat: {
+      options: {
+        banner: '<%= meta.banner %>',
+        stripBanners: true
+      },
+      js: {
+        src: ['jquery.<%= meta.name %>.js'],
+        dest: 'dist/jquery.<%= meta.name %>.js'
+      },
+      css: {
+        src: ['jquery.<%= meta.name %>.css'],
+        dest: 'dist/jquery.<%= meta.name %>.css'
+      },
+      misc: {
+        files: {
+          'dist/LICENSE-GPL': 'LICENSE-GPL',
+          'dist/LICENSE-MIT': 'LICENSE-MIT',
+          'dist/README.md': 'README.md',
+          'dist/CHANGELOG': 'CHANGELOG',
+          'dist/AUTHORS': 'AUTHORS'
+        }
+      },
+      plugin: {
+        options: {
+          banner: '',
+          stripBanners: true
+        },
+        files: {
+          '<%= pkg.name %>.jquery.json': ['package.json']
+        }
+      }
+    },
 
-		// Concat definitions
-		concat: {
-			dist: {
-				src: ["src/jquery.skinswitcher.js"],
-				dest: "dist/jquery.skinswitcher.js"
-			},
-			options: {
-				banner: "<%= meta.banner %>"
-			}
-		},
+    uglify: {
+      options: {
+        banner: '<%= meta.banner %>'
+      },
+      dist: {
+        files: {
+          'dist/jquery.<%= meta.name %>.min.js': ['<%= concat.js.dest %>']
+        }
+      }
+    },
 
-		// Lint definitions
-		jshint: {
-			files: ["src/jquery.skinswitcher.js"],
-			options: {
-				jshintrc: ".jshintrc"
-			}
-		},
+    cssmin: {
+      options: {
+        banner: '<%= meta.banner %>'
+      },
+      dist: {
+        src: ['<%= concat.css.dest %>'],
+        dest: 'dist/jquery.<%= meta.name %>.min.css'
+      }
+    },
 
-		// Minify definitions
-		uglify: {
-			my_target: {
-				src: ["dist/jquery.skinswitcher.js"],
-				dest: "dist/jquery.skinswitcher.min.js"
-			},
-			options: {
-				banner: "<%= meta.banner %>"
-			}
-		},
+    jshint: {
+      options: {
+        bitwise: true,
+        curly: true,
+        eqeqeq: true,
+        forin: true,
+        immed: true,
+        sub: true,
+        boss: true,
+        eqnull: true,
+        indent: 4,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        noempty: true,
+        nonew: true,
+        plusplus: true,
+        quotmark: true,
+        regexp: true,
+        undef: true,
+        unused: true,
+        trailing: true,
+        // relaxing options
+        evil: false,
+        regexdash: true,
+        white: false,
+        // environments
+        browser: true,
+        jquery: true
+      },
+      dist: {
+        options: {
+          globals: {
+            jQuery: false,
+            LazyLoad: false
+          }
+        },
+        files: {
+          src: ['Gruntfile.js', 'package.json', 'jquery.<%= meta.name %>.js']
+        }
+      },
+      test: {
+        options: {
+          globals: {
+            jQuery: false,
+            LazyLoad: false,
+            ok: false
+          }
+        },
+        files: {
+          src: ['test/test.js']
+        }
+      }
+    },
 
-		// CoffeeScript compilation
-		coffee: {
-			compile: {
-				files: {
-					"dist/jquery.skinswitcher.js": "src/jquery.skinswitcher.coffee"
-				}
-			}
-		}
+    csslint: {
+      dist: {
+        src: ['jquery.<%= meta.name %>.css'],
+        options: {
+          'box-model': false
+        }
+      }
+    },
 
-	});
+    copy: {
+      legacy: {
+        files: [
+          {
+            expand: true,
+            cwd: 'dist/',
+            src: ['jquery.<%= meta.name %>.min.js', 'jquery.<%= meta.name %>.min.css'],
+            dest: '.'
+          }
+        ]
+      }
+    },
 
-	grunt.loadNpmTasks("grunt-contrib-concat");
-	grunt.loadNpmTasks("grunt-contrib-jshint");
-	grunt.loadNpmTasks("grunt-contrib-uglify");
-	grunt.loadNpmTasks("grunt-contrib-coffee");
+    compress: {
+      zip: {
+        options: {
+          archive: 'builds/<%= pkg.name %>-<%= pkg.version %>.zip'
+        },
+        files: [{expand: true, cwd: 'dist/', src: ['*']}]
+      }
+    },
 
-	grunt.registerTask("default", ["jshint", "concat", "uglify"]);
-	grunt.registerTask("travis", ["jshint"]);
+    qunit: {
+      all: {
+        options: {
+          urls: [
+            // 'http://localhost:8000/test/index.html?jquery=1.3.2',
+            // 'http://localhost:8000/test/index.html?jquery=1.4.2',
+            'http://localhost:8000/test/index.html?jquery=1.4.3',
+            'http://localhost:8000/test/index.html?jquery=1.5.1',
+            'http://localhost:8000/test/index.html?jquery=1.6.2',
+            'http://localhost:8000/test/index.html?jquery=1.6.4',
+            'http://localhost:8000/test/index.html?jquery=1.7.1',
+            'http://localhost:8000/test/index.html?jquery=1.7.2',
+            'http://localhost:8000/test/index.html?jquery=1.8.3',
+            'http://localhost:8000/test/index.html?jquery=1.9.0',
+            'http://localhost:8000/test/index.html?jquery=1.9.1',
+            'http://localhost:8000/test/index.html?jquery=1.10.1',
+            'http://localhost:8000/test/index.html?jquery=1.10.2',
+            'http://localhost:8000/test/index.html?jquery=2.0.0',
+            'http://localhost:8000/test/index.html?jquery=2.0.1',
+            'http://localhost:8000/test/index.html?jquery=2.0.2',
+            'http://localhost:8000/test/index.html?jquery=2.0.3'
+          ]
+        }
+      }
+    },
+
+    connect: {
+      'default': {}
+    },
+
+    clean: ['dist/']
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-csslint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-compress');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+
+  grunt.registerTask('lint', ['jshint', 'csslint']);
+  grunt.registerTask('min', ['uglify', 'cssmin']);
+  grunt.registerTask('build', ['copy:legacy', 'compress']);
+  grunt.registerTask('default', ['lint', 'concat', 'min', 'build', 'clean']);
+  grunt.registerTask('test', ['default', 'connect', 'qunit']);
 
 };
--- README.md
+++ README.md
@@ -1,114 +1,14 @@
-# jQuery Skinswitcher [![Build Status](https://secure.travis-ci.org/cleentfaar/jquery-skinswitcher.png?branch=master)](https://travis-ci.org/cleentfaar/jquery-skinswitcher)
+# jQuery Skinswitcher [![Build Status](https://travis-ci.org/cleentfaar/jquery-skinswitcher.png?branch=master)](https://travis-ci.org/cleentfaar/jquery-skinswitcher)
 
-### A Jquery plugin to simplify the switching of stylesheets (skins) by the user, thereby changing the primary colors of the website
+A plugin to ease the switching of skins on a website by the user.
 
-This plugin aims to provide a solid base for the many Bootstrap themes around to get their styleswitching a breeze,
-allowing them to focus on the designing aspect. I currently use it for a custom theme in another project, Clevr.
+Please visit http://cleentfaar.github.com/jquery-skinswitcher/ for Documentation, Examples
+and Getting Started information.
 
-## Usage
+## Release History
 
-1. Include jQuery:
-
-	```html
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
-	```
-
-2. Include plugin's code:
-
-	```html
-	<script src="dist/jquery.skinswitcher.min.js"></script>
-	```
-
-3. Call the plugin:
-
-	```javascript
-	$("#Skinswitcher").skinswitcher();
-	```
-
-## Structure
-
-The basic structure of the project is given in the following way:
-
-```
-├── demo/
-│   └── index.html
-├── dist/
-│   ├── jquery.skinswitcher.js
-│   └── jquery.skinswitcher.min.js
-├── src/
-│   ├── jquery.skinswitcher.coffee
-│   └── jquery.skinswitcher.js
-├── .editorconfig
-├── .gitignore
-├── .jshintrc
-├── .travis.yml
-├── skinswitcher.jquery.json
-├── Gruntfile.js
-└── package.json
-```
-
-#### [demo/](https://github.com/cleentfaar/jquery-skinswitcher/tree/master/demo)
-
-Contains a simple HTML file to demonstrate your plugin.
-
-#### [dist/](https://github.com/cleentfaar/jquery-skinswitcher/tree/master/dist)
-
-This is where the generated files are stored once Grunt runs.
-
-#### [src/](https://github.com/cleentfaar/jquery-skinswitcher/tree/master/src)
-
-Contains the files responsible for your plugin, you can choose between JavaScript or CoffeeScript.
-
-#### [.editorconfig](https://github.com/cleentfaar/jquery-skinswitcher/tree/master/.editorconfig)
-
-This file is for unifying the coding style for different editors and IDEs.
-
-> Check [editorconfig.org](http://editorconfig.org) if you haven't heard about this project yet.
-
-#### [.gitignore](https://github.com/cleentfaar/jquery-skinswitcher/tree/master/.gitignore)
-
-List of files that we don't want Git to track.
-
-> Check this [Git Ignoring Files Guide](https://help.github.com/articles/ignoring-files) for more details.
-
-#### [.jshintrc](https://github.com/cleentfaar/jquery-skinswitcher/tree/master/.jshintrc)
-
-List of rules used by JSHint to detect errors and potential problems in JavaScript.
-
-> Check [jshint.com](http://jshint.com/about/) if you haven't heard about this project yet.
-
-#### [.travis.yml](https://github.com/cleentfaar/jquery-skinswitcher/tree/master/.travis.yml)
-
-Definitions for continous integration using Travis.
-
-> Check [travis-ci.org](http://about.travis-ci.org/) if you haven't heard about this project yet.
-
-#### [boilerplate.jquery.json](https://github.com/cleentfaar/jquery-skinswitcher/tree/master/boilerplate.jquery.json)
-
-Package manifest file used to publish plugins in jQuery Plugin Registry.
-
-> Check this [Package Manifest Guide](http://plugins.jquery.com/docs/package-manifest/) for more details.
-
-#### [Gruntfile.js](https://github.com/cleentfaar/jquery-skinswitcher/tree/master/Gruntfile.js)
-
-Contains all automated tasks using Grunt.
-
-> Check [gruntjs.com](http://gruntjs.com) if you haven't heard about this project yet.
-
-#### [package.json](https://github.com/cleentfaar/jquery-skinswitcher/tree/master/package.json)
-
-Specify all dependencies loaded via Node.JS.
-
-> Check [NPM](https://npmjs.org/doc/json.html) for more details.
-
-## Contributing
-
-Check [CONTRIBUTING.md](https://github.com/cleentfaar/jquery-skinswitcher/blob/master/CONTRIBUTING.md)
-
-## History
-
-Check [Release](https://github.com/cleentfaar/jquery-skinswitcher/releases) list.
+See [CHANGELOG](https://github.com/cleentfaar/jquery-skinswitcher/blob/master/CHANGELOG)
 
 ## License
-
-[MIT License](http://cleentfaar.mit-license.org/)
+Copyright (c) 2013 Cas Leentfaar
+Licensed under the MIT, GPL licenses.
