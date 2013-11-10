a405b7b7e4c3d42eeb2ba4745cb67cdb31e8fe61
cleentfaar <leentfaar87@gmail.com>
2013-11-10 01:34:27 +0100

    Removed older jquery versions from Grunt testing

--- Gruntfile.js
+++ Gruntfile.js
@@ -165,17 +165,6 @@ module.exports = function(grunt) {
       all: {
         options: {
           urls: [
-            // 'http://localhost:8000/test/index.html?jquery=1.3.2',
-            // 'http://localhost:8000/test/index.html?jquery=1.4.2',
-            'http://localhost:8000/test/index.html?jquery=1.4.3',
-            'http://localhost:8000/test/index.html?jquery=1.5.1',
-            'http://localhost:8000/test/index.html?jquery=1.6.2',
-            'http://localhost:8000/test/index.html?jquery=1.6.4',
-            'http://localhost:8000/test/index.html?jquery=1.7.1',
-            'http://localhost:8000/test/index.html?jquery=1.7.2',
-            'http://localhost:8000/test/index.html?jquery=1.8.3',
-            'http://localhost:8000/test/index.html?jquery=1.9.0',
-            'http://localhost:8000/test/index.html?jquery=1.9.1',
             'http://localhost:8000/test/index.html?jquery=1.10.1',
             'http://localhost:8000/test/index.html?jquery=1.10.2',
             'http://localhost:8000/test/index.html?jquery=2.0.0',
